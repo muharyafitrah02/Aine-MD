7a907d2a84c070f712c9fe627a9ed6b6f5125b33
muharyafitrah02 <108320846+muharyafitrah02@users.noreply.github.com>
2022-07-08 19:04:41 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • Dana,Gopay,Pulsa 」
+│ •  [0852-4434-4615]
+│ •
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
